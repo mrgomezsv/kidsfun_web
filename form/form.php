9c2261fde1fe05ea6c1a3de7ec7611138cf8cdd3
Mario Roberto Gomez <mrg@treming.com>
2023-06-24 15:15:04 -0600

    corrección de php form

--- form/form.php
+++ form/form.php
@@ -1,19 +1,22 @@
-
 <?php
-if($_POST) {
+if ($_POST) {
   $nombre1 = $_POST['firstName'];
   $nombre2 = $_POST['lastName'];
-  $numContacto= $_POST['contactNumber'];
+  $numContacto = $_POST['contactNumber'];
   $correo = $_POST['email'];
   $mensaje = $_POST['reason'];
 
   $destinatario = "mrgomez.dev@gmail.com";
   $asunto = "Mensaje enviado desde mi sitio web";
-  $contenido = "Nombre: " . $nombre1 . "Apellido: " . $nombre2 . "Telefono: " . $contactNumber . "\nCorreo electrónico: " . $email . "\nMensaje: " . $reason;
+  $contenido = "Nombre: " . $nombre1 . " Apellido: " . $nombre2 . " Teléfono: " . $numContacto . "\nCorreo electrónico: " . $correo . "\nMensaje: " . $mensaje;
 
-  $header = "From: " . $email . "\r\n";
-  $header .= "Reply-To: " . $email . "\r\n";
+  $header = "From: " . $correo . "\r\n";
+  $header .= "Reply-To: " . $correo . "\r\n";
 
-  mail($destinatario, $asunto, $contenido, $header);
+  if (mail($destinatario, $asunto, $contenido, $header)) {
+    echo "Gracias por contactarnos. Tu mensaje ha sido enviado.";
+  } else {
+    echo "Lo sentimos, ha ocurrido un error al enviar tu mensaje.";
+  }
 }
 ?>
